06e4f13c3807d3945aae74ee06f8777d07db2572
BerrocalG <guadalupe.berrocal@usil.pe>
2025-04-06 17:36:14 -0500

    ensayo 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,43 @@
+//codigo para motor 2 pies pd4 pd3
+#define F_CPU 16000000UL  
+#include <avr/io.h>  
+#include <util/delay.h> 
+
+int main (void){
+    DDRD|=0X0C;
+    PORTD&=~(0X0C);
+    DDRD&=~(0X20);
+    PORTD|=(0X20);
+    char flag=0;
+
+    while(1) {
+
+        if(!(PIND&0X20)){ //cuando el pin del boton este en 0 se va a prender
+            _delay_ms(100);
+
+            switch(flag){
+                case 0: //caso 0
+
+                PORTD|=0X08; //cuando el pd7 esta en 1
+                flag=1; //estado1
+                break; // para que solo ejecute ese bloque
+
+                case 1:
+                PORTD&=~(0X0C); //cuando el pd7 y pd6 esten en 1 se va negar y se volvera 0 significando que esta apagado
+                _delay_ms(50); //abra un delay
+                PORTD|=(0X04); //el pin pd6 va a estar en 1 girando en otro sentido
+                flag=2; //estado 2
+                break; //para solo ejecutar caso1
+
+                case 2: //solo esn ombre de estado
+                PORTD&=~(0X0C); //apagados pd7 y pd6
+                flag=0; // se va volver al estado 0
+                break; //no seguir ejecutando
+            }
+        }
+    } 
+}
+/*
 //codigo base clase para 1 motor
 #define F_CPU 16000000UL  
 #include <avr/io.h>  
@@ -34,4 +74,6 @@ int main (void){
                 break;
             }
         }
-    }
+    } 
+}
+*/
