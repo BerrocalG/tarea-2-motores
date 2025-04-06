40bab62cf5113aaa8100c41459c5bd1987af6680
BerrocalG <guadalupe.berrocal@usil.pe>
2025-04-06 17:40:44 -0500

    uniendo los dos motores con 1 boton

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
-//codigo para motor 2 pies pd4 pd3
+//codigo uniendo los dos motores funcionen al mismo tiempo con 1 boton
 #define F_CPU 16000000UL  
 #include <avr/io.h>  
 #include <util/delay.h> 
 
 int main (void){
+    //motor 1
+    DDRD|=0XC0;
+    PORTD&=~(0XC0);
+    //motor 2
     DDRD|=0X0C;
     PORTD&=~(0X0C);
+
+    //boton
     DDRD&=~(0X20);
     PORTD|=(0X20);
     char flag=0;
@@ -17,20 +23,23 @@ int main (void){
 
             switch(flag){
                 case 0: //caso 0
-
+                PORTD|=0X80;
                 PORTD|=0X08; //cuando el pd7 esta en 1
                 flag=1; //estado1
                 break; // para que solo ejecute ese bloque
 
                 case 1:
                 PORTD&=~(0X0C); //cuando el pd7 y pd6 esten en 1 se va negar y se volvera 0 significando que esta apagado
+                PORTD&=~(0XC0);
                 _delay_ms(50); //abra un delay
                 PORTD|=(0X04); //el pin pd6 va a estar en 1 girando en otro sentido
+                PORTD|=(0X40);
                 flag=2; //estado 2
                 break; //para solo ejecutar caso1
 
                 case 2: //solo esn ombre de estado
                 PORTD&=~(0X0C); //apagados pd7 y pd6
+                PORTD&=~(0XC0);
                 flag=0; // se va volver al estado 0
                 break; //no seguir ejecutando
             }
