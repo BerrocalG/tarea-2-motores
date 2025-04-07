548356848356b452f73fe439178de6afa5daa4ff
BerrocalG <guadalupe.berrocal@usil.pe>
2025-04-07 12:41:01 -0500

    codigo ejercicio de clase probado con if

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,43 @@
 
+//codigo clase un motor giro derecha izquierda con pulsador
+#define F_CPU 16000000UL  
+#include <avr/io.h>  
+#include <util/delay.h> 
+
+int main (void){
+    DDRD|=(0XC0);
+    PORTD&=~(0XC0);
+    DDRD&=~(0X20);
+    PORTD|=(0X20);
+    char flag=0;
+
+    while(1){
+    if(!(PIND&0x20)){
+        _delay_ms(100);
+
+        if(flag==0){
+            PORTD|=0X80;
+            flag=1;
+        }
+        else{
+            if(flag==1){
+                PORTD&=~(0XC0);
+                _delay_ms(50);
+                PORTD|=(0X40);
+                flag=2;
+
+            }
+            else{
+                PORTD&=~(0XC0);
+                flag=0;
+            }
+        }
+    }    
+    
+}
+}
+
+/*
 //codigo uniendo los dos motores funcionen al mismo tiempo con 1 boton
 #define F_CPU 16000000UL  
 #include <avr/io.h>  
@@ -6,7 +45,7 @@
 
 int main (void){
     //motor 1
-    DDRD|=0XC0;
+    DDRD|=0XC0; 
     PORTD&=~(0XC0);
 
     //motor 2
@@ -92,44 +131,4 @@ int main (void){
     } 
 }
 
-
-/*
-//codigo base clase para 1 motor
-#define F_CPU 16000000UL  
-#include <avr/io.h>  
-#include <util/delay.h> 
-
-int main (void){
-    DDRD|=0XC0;
-    PORTD&=~(0XC0);
-    DDRD&=~(0X20);
-    PORTD|=(0X20);
-    char flag=0;
-
-    while(1) {
-
-        if(!(PIND&0X20)){
-            _delay_ms(100);
-            switch(flag){
-                case 0:
-
-                PORTD|=0X80;
-                flag=1;
-                break;
-
-                case 1:
-                PORTD&=~(0XC0);
-                _delay_ms(50);
-                PORTD|=(0X40);
-                flag=2;
-                break;
-
-                case 2:
-                PORTD&=~(0XC0);
-                flag=0;
-                break;
-            }
-        }
-    } 
-}
 */
